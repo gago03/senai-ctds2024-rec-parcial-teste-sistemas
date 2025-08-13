ddc6760aa953b4f90f73fb14a1058d3eef3f9761
gago03 <christianvicaroni12@gmail.com>
2025-08-12 21:26:33 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import FormularioCadastroRP from './FormularioCadastroRP'
+
+
+function App() {
+  return (
+    <>
+      <FormularioCadastroRP/>
+      
+    </>
+  )
+
+}
+
+
+export default App
+
